30217989a627887febc70532cf639bddd36f7865
Yuri Ratkevich <ura_rat@mail.ru>
2022-12-25 22:37:18 +0300

    Update next config and gitlab-ci

--- next.config.cjs
+++ next.config.cjs
@@ -3,17 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: {
     outputStandalone: true,
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      issuer: { and: [/\.(js|ts)x?$/] },
-
-      use: ['@svgr/webpack', 'url-loader'],
-    });
-
-    return config;
-  },
+  }
 }
 
 module.exports = nextConfig
