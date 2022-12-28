1ba49ff28e242a1384a589d04aa6e37c223f6652
Yuri Ratkevich <ura_rat@mail.ru>
2022-12-28 16:12:51 +0300

    Build fix

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,17 +1,10 @@
-﻿import styles from '../styles/Header.module.css';
-import IPost from "../src/models/IPost";
+﻿import IPost from "../src/models/IPost";
+// import styles from '../styles/Post.module.css'
 
 export default function Post(props: IPost) {
     return (
         <>
-            <div className={styles.header}>
-               <p>{props._id.toString()}</p>
-               <p>{props.content.toString()}</p>
-               <p>{props.publishDate.toString()}</p>
-               <p>{props.creationDate.toString()}</p>
-               <p>{props.sent.toString()}</p>
-               <p>{props.sender.toString()}</p>
-            </div>
+            {/*<p className={styles.text}>{props._id.toString()}</p>*/}
         </>
     )
 }
